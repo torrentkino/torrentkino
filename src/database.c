92b0ea6d8d5463fe33adea4fe03c6b4e5280f15f
Aiko Barz <aiko@chroot.de>
2013-05-24 23:14:54 +0200

    Cleanup

--- src/database.c
+++ src/database.c
@@ -1,20 +1,20 @@
 /*
 Copyright 2011 Aiko Barz
 
-This file is part of Torrentkino.
+This file is part of masala.
 
-Torrentkino is free software: you can redistribute it and/or modify
+masala is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Torrentkino is distributed in the hope that it will be useful,
+masala is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Torrentkino.  If not, see <http://www.gnu.org/licenses/>.
+along with masala.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <stdio.h>
