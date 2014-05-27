2d9765f6fb7b9e2b144babd342df0e727534e957
Aiko Barz <aiko@chroot.de>
2014-05-27 18:34:28 +0200

    Manpage cleanup

--- bin/docs.sh
+++ bin/docs.sh
@@ -6,14 +6,12 @@ fi
 
 if [ "x$1" = "xtorrentkino" ]; then
 
-	ronn < README.md > debian/docs/torrentkino6.1
-	ronn < README.md > debian/docs/torrentkino4.1
-	ronn < README.md > debian/docs/tk.1
+	ronn < README.md > debian/docs/tk6.1
+	ronn < README.md > debian/docs/tk4.1
 
 	cat > debian/torrentkino.manpages <<EOF
-./debian/docs/torrentkino6.1
-./debian/docs/torrentkino4.1
-./debian/docs/tk.1
+./debian/docs/tk6.1
+./debian/docs/tk4.1
 EOF
 
 elif [ "x$1" = "xtumbleweed" ]; then
